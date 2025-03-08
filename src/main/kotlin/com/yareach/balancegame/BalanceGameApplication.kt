61c3b7a2892a85a570cb9e8d7084cb3b053a1029
yareach <mine8345@naver.com>
2025-03-09 00:39:38 +0900

    init

---
+++ src/main/kotlin/com/yareach/balancegame/BalanceGameApplication.kt
@@ -0,0 +1,11 @@
+package com.yareach.balancegame
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BalanceGameApplication
+
+fun main(args: Array<String>) {
+    runApplication<BalanceGameApplication>(*args)
+}
---
+++ src/test/kotlin/com/yareach/balancegame/BalanceGameApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.yareach.balancegame
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BalanceGameApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
